72e81b51103b4cd7e3a1a8da0d3cdec607c5e550
Frank-Zou <1654209516@qq.com>
2019-10-12 00:19:22 -0700

    Submission for Module 1

---
+++ IN/YunshengZou/Module1/data_types/primitives.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    signed int num = 300000000;//change the int to signed int
+    bool myBool = false;//change the myBool value - true to false
+    cout<<"My number is:"<<num<<endl;
+    cout<<"True is represented as:"<<myBool<<endl;
+}
