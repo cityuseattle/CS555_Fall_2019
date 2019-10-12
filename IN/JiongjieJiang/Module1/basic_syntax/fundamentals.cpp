f7fd901ede26515d60dd66b62e7de32ef57ae5f5
Triple_J <joker4jax@gmail.com>
2019-10-12 15:41:22 -0700

    Submission for Module 1

---
+++ IN/JiongjieJiang/Module1/basic_syntax/fundamentals.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //iostream provides basic input and output services for C++ programs
+
+using namespace std; //do not type std:: prefix
+
+int main() // a global function named main
+{
+    cout << "Comments"; // Output string “Comments”
+    return 0; //return an integer value, terminate the main function
+} //curly braces: Start and end of the function
