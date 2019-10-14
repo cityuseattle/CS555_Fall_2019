593e14367fcd4a5234874a11b77b353b37ad96f5
Apiwat Chuaphan <achuaphan@gmail.com>
2019-10-14 11:16:21 -0700

    Submission for Module 1

---
+++ IN/ApiwatChuaphan/Module1/variable_scope/scope.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int g;
+
+int main()
+{
+    int a, b;
+    int c;
+
+    a = 10;
+    b = 20;
+    g = a + b;
+
+    cout << g << endl;
+    return 0;
+}
