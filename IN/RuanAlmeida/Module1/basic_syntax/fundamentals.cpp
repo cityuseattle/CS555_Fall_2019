670a0e2f7422feedddcb6ed818754b43a048dff7
ruanmurta <ruanmurta@hotmail.com>
2019-10-17 20:54:36 -0700

    partial submission for module 1

---
+++ IN/RuanAlmeida/Module1/basic_syntax/fundamentals.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // please explain this line
+using namespace std; // please explain this line
+
+// please explain main
+int main() {
+    cout << "Comments"; // please explain this cout
+    return 0;           // please explain this return
+}                       // please explain }
---
+++ IN/RuanAlmeida/Module1/variable_types/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+   int i, j, k;
+   string c, ch;
+
+   float f, salary;
+   double d;
+
+   k = 555;
+   c = "C";
+   ch = "S";
+   cout << c + ch + to_string(k);
+   // return 0;           
+}             
