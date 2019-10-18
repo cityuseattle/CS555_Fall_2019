24431a72b19e21e43daaab96f05a20f5915d510d
dasaryvineeth <dasaryvineeth@gmail.com>
2019-10-17 18:26:39 -0700

    modified access file with additional box class

--- IN/VineethDasary/Module1/classes_and_objects/access.cpp
+++ IN/VineethDasary/Module1/classes_and_objects/access.cpp
@@ -4,21 +4,31 @@ using namespace std;
 
 class Box {
     public:
-        double lenghth;
+        double length;
         double breadth;
         double height;
 };
 
 int main() {
     Box Box1;
+    Box VineethBox;
+
     double volume = 0.0;
+    double volume1 = 0.0;
 
     Box1.height = 5.0;
-    Box1.lenghth = 6.0;
+    Box1.length = 6.0;
     Box1.breadth = 7.0;
 
-    volume = Box1.height * Box1.lenghth * Box1.breadth;
+    VineethBox.height = 10.0;
+    VineethBox.length = 11.0;
+    VineethBox.breadth = 12.0;
+
+    volume = Box1.height * Box1.length * Box1.breadth;
     cout << "Volume of Box1 :" << volume << endl;
+
+    volume1 = VineethBox.height * VineethBox.length * VineethBox.breadth;
+    cout << "Vineeth Box volume :" << volume1 << endl;
 
     return 0;
 }
