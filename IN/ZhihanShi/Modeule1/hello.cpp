37974681ee400108cf1403e88581725b873b9d8a
ZHIHAN SHI <zhihanshi@ZHIHANdeMacBook-Pro.local>
2019-10-18 19:02:30 -0700

    Submission for Module 2

--- IN/ZhihanShi/Modeule1/hello.cpp
+++ IN/ZhihanShi/Modeule1/hello.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    int a ;
-    
-    cout << "please choice 1 or 2"<< endl;
-    cin >> a;
-    switch (a)
+    int a ;             // Judging variable
+    double length;      // I use double to prevent the user from entering a decimal to cause an inability to operate.
+    double width;
+    double area;
+    double radius;
+    for(;;)             //Infinite loop. The exit is case 3.
     {
-    case 1:
-        cout << "choice 1"<< endl;
-        break;
-    case 2:
-        cout << "choice 2"<< endl;
-        break;
-    default:
-        break;
+        cout << "please choice rectangle(1) or circle (2) or exit(3)"<< endl;
+        cin >> a;
+        switch (a)      //I use switch because it is rarely judged and it looks beautiful.
+        {
+        case 1:         //rectangle
+            cout << "Please enter length: "<<endl;
+            cin >> length; // I have a problem that cannot be solved which is when the user used is, if they entered string. How to solve it?
+            cout << "Please enter width: "<<endl;
+            cin >> width;
+            area = length*width;
+            cout << "The area is :"<< area <<endl;
+            break;
+        case 2:         //circle
+            cout << "Please enter radius:"<< endl;
+            cin >> radius;
+            area = radius*radius*3.14;
+            cout <<"The area is :"<< area <<endl;
+            break;
+        case 3:         // the  exit of loop
+            cout << "Thank you for using!"<< endl;
+            return 0;
+        default:
+            cout << "Wrong enter, please try again" << endl;
+            break;
+        }
     }
-    cout << "hello"<< endl;
+    
 }
+
