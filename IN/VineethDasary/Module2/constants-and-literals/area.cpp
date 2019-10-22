f085467887fc54cd7d55318ea274083a1a6033cf
dasaryvineeth <dasaryvineeth@gmail.com>
2019-10-21 18:40:13 -0700

    Updated Constants and Literals

--- IN/VineethDasary/Module2/constants-and-literals/area.cpp
+++ IN/VineethDasary/Module2/constants-and-literals/area.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 #define NEWLINE '\n'
+#define HTAB '\t'
 
 int main() {
     const int LENGTH = 10;
     const int WIDTH = 5;
+    const char VTAB = '\v';
     int area;
 
     area = LENGTH * WIDTH;
-    cout << "area: " << area;
+    cout << "area:" << HTAB << VTAB << area;
     cout << NEWLINE;
     return 0;
 }
