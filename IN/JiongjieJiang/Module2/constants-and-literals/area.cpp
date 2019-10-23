4995f8f6978a42846019ba95ebd8a6bfa696b5f0
Triple_J <joker4jax@gmail.com>
2019-10-23 16:03:02 -0700

    Submission for Module 2 (modified)

--- IN/JiongjieJiang/Module2/constants-and-literals/area.cpp
+++ IN/JiongjieJiang/Module2/constants-and-literals/area.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 #define NEWLINE '\n'
+#define HORIZONTAL '\t'
 
 int main()
 {
     const int LENGTH = 10;
     const int WIDTH = 5;
-    const string VerticalTab = "\t";
+    const string VerticalTab = "\v";
     int area;
 
     area = LENGTH * WIDTH;
-    cout << "Area:" << NEWLINE << VerticalTab << area;
+    cout << "Area:" << HORIZONTAL << VerticalTab << area;
 
     return 0;
 }
