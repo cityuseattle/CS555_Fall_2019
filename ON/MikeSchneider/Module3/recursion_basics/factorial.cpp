15cacc8aa26a568ad8cc927115256b90e22605de
MikeSchneider <schneidermichael@cityuniversity.edu>
2019-10-26 17:04:40 -0700

    Submission for Module 3

---
+++ ON/MikeSchneider/Module3/recursion_basics/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long int factorial(unsigned int i){
+
+    if(i <= 1){
+        return 0;
+    }
+    return i * factorial(i - 1);
+}
+
+int main(){
+    int i = 12;
+    cout << "Factorial of " << i << " is " << factorial(i);
+    cout << endl;
+
+    return 0;
+}
