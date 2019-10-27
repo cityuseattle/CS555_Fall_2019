9fa3bd5cf3c08f85f75e8b2b465249ba711cc003
PJB84 <patrickjbeck84@gmail.com>
2019-10-27 08:29:11 -0700

    Submission for Module 3

---
+++ ON/PatrickBeck/Module3/recursion_basics/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long int factorial(unsigned int i) {
+
+    if(i == 0) {
+        return 1;
+    }
+    else
+        return i * factorial(i - 1);
+    
+}
+
+int main() {
+    int i = 12;
+    cout << "Factorial of " << i << " is " << factorial(i);
+    cout << endl;
+    return 0;
+}
