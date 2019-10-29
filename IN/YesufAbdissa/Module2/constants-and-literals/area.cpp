240eac49126dac7aded93e055f5c817b92ff289a
Yesuf <46637371+yuyeabdissa@users.noreply.github.com>
2019-10-28 17:05:43 -0700

    Submission for Module2

---
+++ IN/YesufAbdissa/Module2/constants-and-literals/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define NEWLINE '\n'
+#define Horizontal Tab '\t'
+
+int main() {
+    const int LENGTH = 10;
+    const int WIDTH = 5;
+    int area;
+
+    area = LENGTH * WIDTH;
+    cout << "Area: =\v " << area << endl;
+    return 0;
+    
+}  
