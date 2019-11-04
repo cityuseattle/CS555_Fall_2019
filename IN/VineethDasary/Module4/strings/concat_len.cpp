c069debabda8ac4391c33a3b8d92d0f03b090700
dasaryvineeth <dasaryvineeth@gmail.com>
2019-11-04 11:34:17 -0800

    concat_len

---
+++ IN/VineethDasary/Module4/strings/concat_len.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+    char str1[10] = "Hello";
+    char str2[10] = "World";
+
+    strcat(str1, str2);
+    cout << "strcat(str1, str2): " << str1 <<endl;
+
+    return 0;
+}
