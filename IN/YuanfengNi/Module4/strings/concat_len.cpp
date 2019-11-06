2f92a533b5122ec261721f7cf289806c4300edb1
园丰 <yuanfeng@yuanfengdeMacBook-Pro.local>
2019-11-06 14:21:41 -0800

    Submission for Module 4

---
+++ IN/YuanfengNi/Module4/strings/concat_len.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    char str1[11]="Hello";
+    char str2[10]="World";
+    strcat(str1,str2);
+    cout<<"strcat(str1, str2 ): "<<str1<<endl;
+    cout<<"strlen(str1): "<<strlen(str1)<<endl;
+    return 0;
+}
