e66fe067beed0e1bb0c4a50c4de9aba4bda14c14
ruanmurta <ruanmurta@hotmail.com>
2019-11-06 23:51:57 -0800

    adding and commiting Module4 files

---
+++ IN/RuanAlmeida/Module4/date_and_time/curr_date.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    time_t now = time(0);
+
+    char* dt = ctime(&now);
+
+    cout << "the local date and time is: " << dt <<endl;
+
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    cout << "THe UTC date and time is: " << dt << endl;
+}
