1c7fc1f0eee1272e12375c5d91cb9f76357f8ff1
Triple_J <joker4jax@gmail.com>
2019-11-10 22:18:51 -0800

    Submission for Module 5

---
+++ IN/JiongjieJiang/Module5/overloading/overload_print.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class printData
+{
+public:
+    void print(int i)
+    {
+        cout << "Pringting int: " << i << endl;
+    }
+    void print(double f)
+    {
+        cout << "Printing double: " << f << endl;
+    }
+    void print(char *c)
+    {
+        cout << "Printing character: " << c << endl;
+    }
+};
+
+int main(void)
+{
+    printData pd;
+    pd.print(5);
+    pd.print(500.263);
+    pd.print("Hello C++");
+
+    return 0;
+}
