5f01b78e47e12312742f2c36e2d0ac024afa66ea
Amrutha Vaidyanathan <amrunathan@gmail.com>
2019-11-13 19:25:29 -0800

    Submission for Module5

---
+++ IN/AmruthaV/Module5/dynamic_memory/new_delete.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Box{
+    public:
+    Box(){
+        cout << "Constructor called !" << endl;
+    }
+    ~Box(){
+        cout << "Destractor called !" << endl;
+    }
+};
+
+int main(){
+    double* pvalue =NULL;
+    pvalue = new double;
+
+    *pvalue = 29494.99;
+    cout << "value of pvalue:  "<< *pvalue << endl;
+
+    delete pvalue;
+
+    Box* myBoxArray=new Box[4];
+    delete [] myBoxArray;
+    return 0; 
+}
