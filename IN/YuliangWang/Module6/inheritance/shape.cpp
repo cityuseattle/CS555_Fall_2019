c400fc5f968eb0ab16473ba39c4c103efb9b16e5
yuliangwang03 <43104837+yuliangwang03@users.noreply.github.com>
2019-11-13 16:56:40 -0800

    module6

---
+++ IN/YuliangWang/Module6/inheritance/shape.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+    public: 
+    void setWidth(int w)
+    {
+        width = w;
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
+
+    protected:
+        int width;
+        int height;
+};
+
+class Rectangle: public Shape
+{
+    public:
+        int getArea()
+        {
+            return(width *height);
+        }
+    
+};
+class Square: public Shape
+{
+    public:
+        int getArea()
+        {
+            return(width*width);
+        }
+};
+
+int main(void)
+{
+    Rectangle Rect;
+
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+
+    cout<<"Total area"<< Rect.getArea()<<endl;
+
+    Square Sq;
+    Sq.setWidth(10);
+    cout<<"The Square area is: "<< Sq.getArea()<<endl;
+    return 0;
+}
