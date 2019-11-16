572bfb62cf1fce4527703963b7ddf1f838c38feb
jakeqwe100 <455508203@qq.com>
2019-11-15 21:43:51 -0800

    Module6

---
+++ IN/Zhijian-He/Module6/inheritance/shape.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+    public:
+    void setWidth (int w) {
+        width = w;
+    }
+    void setHeight(int h) {
+        height = h;
+    }
+
+    protected:
+        int width;
+        int height;
+};
+
+class Rectangle: public Shape {
+    public:
+        int getArea() {
+            return (width * height);
+        }
+};
+
+class Square: public Shape {
+    public:
+        int getArea() {
+            return (width * height);
+        }
+};
+
+int main(void) {
+    Rectangle Rect;
+    Square Sq;
+
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+    Sq.setWidth(10);
+    Sq.setHeight(10);
+
+    cout << "Total area: " << Rect.getArea() << endl;
+    cout << "Total area: " << Sq.getArea() << endl;
+
+    return 0;
+}
