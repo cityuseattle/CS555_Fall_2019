6c9d882819b12b72811d7ae9a3a818f5c610bd71
dasaryvineeth <dasaryvineeth@gmail.com>
2019-11-18 22:26:49 -0800

    Updated Square

---
+++ IN/VineethDasary/Module6/inheritance/shape.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+    public:
+        void setWidth(int w) {
+            width = w;
+        }
+        void setHeight(int h) {
+            height = h;
+        }
+
+    protected:
+        int width;
+        int height;
+};
+
+class Rectangle: public Shape {
+    public:
+        int getArea() {
+            return (width * height);
+        }
+};
+
+class Square: public Shape {
+    public:
+        int getArea() {
+            return (width * width);
+        }
+};
+int main(void) {
+    Rectangle Rect;
+    Square Sq;
+
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+    Sq.setWidth(10);
+
+    cout << "Total Area of Rectangle: " << Rect.getArea() << endl;
+    cout << "Total Area of Square: " << Sq.getArea() << endl;
+    return 0;
+
+}
