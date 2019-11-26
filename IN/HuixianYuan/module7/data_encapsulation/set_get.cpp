2129baf54bf10c494af879773547b5f025785a45
EuniceYuan <yhx710364284@gmail.com>
2019-11-25 21:57:30 -0800

    submission for module 7

---
+++ IN/HuixianYuan/module7/data_encapsulation/set_get.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class encapsulation{
+    private:
+    int x;
+
+    public:
+    void set(int a){
+        x=a;
+    }
+    int get(){
+        return x;
+    }
+};
+
+int main(){
+    encapsulation obj;
+
+    obj.set(5);
+    cout<<obj.get()<<endl;
+
+    return 0;
+}
