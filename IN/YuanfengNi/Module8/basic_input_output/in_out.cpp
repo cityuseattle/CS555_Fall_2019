bf66aced66777e41a59e8e061976289551774abc
园丰 <yuanfeng@yuanfengdeMacBook-Pro.local>
2019-12-03 00:03:02 -0800

    Submission for Module 8

---
+++ IN/YuanfengNi/Module8/basic_input_output/in_out.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    char name[50];
+    char str[] = "Unable to read....";
+    cout<<"Please enter your name: ";
+    cin>>name;
+    cout<<"Your name is: "<<name<<endl;
+     cout<<"Please enter your age: ";
+    cin>>age;
+    cout<<"Your age is: "<<age<<endl;
+    cout<<"Error message: "<<str<<endl;
+    cout<<"Error message: "<<str<<endl;
+}
