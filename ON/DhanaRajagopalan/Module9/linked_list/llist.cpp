e3166d0579d95ca18633b1de4a154c76d43894a6
#City2019 <dhanasnkl@gmail.com>
2019-12-05 20:03:14 -0800

    Submission for Module 9

---
+++ ON/DhanaRajagopalan/Module9/linked_list/llist.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+  int data;
+  Node* next;
+};
+
+// This function prints contents of linked list
+// starting from the given node
+void printList(Node* n)
+{
+  while (n != NULL) {
+    cout << n->data << " ";
+    n = n->next;
+  }
+}
+
+void insertAfter(struct Node* prev_node, int new_data) 
+{ 
+    /*1. check if the given prev_node is NULL */ 
+    if (prev_node == NULL)  
+    {  
+       printf("the given previous node cannot be NULL");        
+       return;   
+    }   
+           
+    /* 2. allocate new node */
+    struct Node* new_node =(struct Node*) malloc(sizeof(struct Node)); 
+   
+    /* 3. put in the data  */
+    new_node->data  = new_data; 
+   
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;  
+   
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node; 
+} 
+
+// Driver code
+int main()
+{
+  Node* head = NULL;
+  Node* second = NULL;
+  Node* third = NULL;
+
+  // allocate 3 nodes in the heap
+  head = new Node();
+  second = new Node();
+  third = new Node();
+
+  head->data = 1; // assign data in first node
+  head->next = second; // Link first node with second
+
+  second->data = 2; // assign data to second node
+  second->next = third;
+
+  third->data = 3; // assign data to third node
+  third->next = NULL;
+
+  insertAfter(head->next, 8);
+
+  printList(head);
+
+  return 0;
+}
