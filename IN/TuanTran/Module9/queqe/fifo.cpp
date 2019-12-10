498a87867980ed2389d0e91fc5464638a6063681
tuankhaitran <trankhaituan112@gmail.com>
2019-12-10 13:26:03 -0800

    Submission for Module 9

---
+++ IN/TuanTran/Module9/queqe/fifo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue> 
+using namespace std;
+
+void showq(queue <int> gq)
+{
+    queue <int> g = gq;
+    while(!g.empty())
+    {
+        cout <<'\t'<<g.front();
+        g.pop();
+    }
+}
+
+int main()
+    {
+        queue <int> gquiz;
+        gquiz.push(10);
+        gquiz.push(20);
+        gquiz.push(30);
+        cout << "The queue gquiz is : ";
+        showq(gquiz);
+
+        cout <<"\ngquiz.size() :"<<gquiz.size();
+        cout <<"\ngquiz.front() :"<<gquiz.size();
+        cout <<"\ngquiz.back() :"<<gquiz.size();
+    
+        cout <<"\ngquiz.pop() : ";
+        gquiz.pop();
+        showq(gquiz);
+        cout << endl;
+        return 0;
+    }
+
