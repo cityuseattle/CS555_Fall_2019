27bc8ad2a3c8c3cb4abab1432cdbfea2dd206a94
Amrutha Vaidyanathan <amrunathan@gmail.com>
2019-12-10 12:26:36 -0800

    Submission for Module 10

--- IN/AmruthaV/Module9/linked_list/llist.cpp
+++ IN/AmruthaV/Module9/linked_list/llist.cpp
@@ -1,72 +1,53 @@
-#include <iostream>
+#include<iostream>
+#include<math.h>
 using namespace std;
-
-class Node {
-public:
-    int data;
-    Node *next;
-};
-
-void printList(Node *n)
-{
-    while (n != NULL) {
-        cout << n->data << " ";
-        n = n->next;
-    }
-    cout << endl;
+// find gcd
+int gcd(int a, int b) {
+   int t;
+   while(1) {
+      t= a%b;
+      if(t==0)
+      return b;
+      a = b;
+      b= t;
+   }
 }
-
-void addNode(Node *head, Node *newNode)
-{
-    if (head != NULL && newNode != NULL)
-    {
-        if (head->next == NULL)
-        {
-            head->next = newNode;
-        }
-        else
-        {
-            Node *curr = head->next;
-            while (curr->next != NULL)
-            {
-                curr = curr->next;
-            }
-            curr->next = newNode;
-        }
-    }
-    
-}
-
-int main()
-{
-    Node *head = NULL;
-    Node *second = NULL;
-    Node *third = NULL;
-
-    head = new Node();
-    second = new Node();
-    third = new Node();
-
-    head->data = 1;
-    head->next = second;
-
-    second->data = 2;
-    second->next = third;
-
-    third->data = 3;
-    third->next = NULL;
-
-    printList(head);
-
-    Node *newNode = NULL;
-    newNode = new Node();
-    newNode->data = 4;
-    newNode->next = NULL;
-
-    cout << "Inserting a new node at the end of the list" << endl;
-    addNode(head, newNode);
-
-    printList(head);
-
-    return 0;
+int main() {
+   //2 random prime numbers
+   double p = 13;
+   double q = 11;
+   double n=p*q;//calculate n
+   double track;
+   double phi= (p-1)*(q-1);//calculate phi
+   //public key
+   //e stands for encrypt
+   double e=7;
+   //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
+   while(e<phi) {
+      track = gcd(e,phi);
+      if(track==1)
+         break;
+      else
+         e++;
+   }
+   //private key
+   //d stands for decrypt
+   //choosing d such that it satisfies d*e = 1 mod phi
+   double d1=1/e;
+   double d=fmod(d1,phi);
+   double message = 9;
+   double c = pow(message,e); //encrypt the message
+   double m = pow(c,d);
+   c=fmod(c,n);
+   m=fmod(m,n);
+   cout<<"Original Message = "<<message;
+   cout<<"\n"<<"p = "<<p;
+   cout<<"\n"<<"q = "<<q;
+   cout<<"\n"<<"n = pq = "<<n;
+   cout<<"\n"<<"phi = "<<phi;
+   cout<<"\n"<<"e = "<<e;
+   cout<<"\n"<<"d = "<<d;
+   cout<<"\n"<<"Encrypted message = "<<c;
+   cout<<"\n"<<"Decrypted message = "<<m;
+   return 0;
 }
