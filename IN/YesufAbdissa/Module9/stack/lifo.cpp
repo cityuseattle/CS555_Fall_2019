52e12b6c588987898be1cc2f041dbfd8808c5abd
Yesuf <46637371+yuyeabdissa@users.noreply.github.com>
2019-12-10 22:15:03 -0800

    Submission for Module 9

---
+++ IN/YesufAbdissa/Module9/stack/lifo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void showstack(stack <int> s)
+{
+    while (!s.empty())
+    {
+        cout << s.top() << '\t' ;
+        s.pop();
+    }
+    
+if (s.empty()) {
+    cout << "True";
+        }
+    else
+        {
+            cout << "False";
+        }
+    
+}
+
+
+int main(){
+    stack <int> s;
+    s.push(10);
+    s.push(30);
+    s.push(20);
+    s.push(5);
+    s.push(1);
+
+    cout << "The stack is: ";
+    showstack(s);
+
+    cout << "\ns.size() : " << s.size();
+    cout << "\ns.top(): " << s.top();
+
+    cout << "\ns.pop() : ";
+    s.pop();
+    showstack(s);
+    cout << endl;
+
+    return 0;
+ }
